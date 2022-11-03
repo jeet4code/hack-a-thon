26c82db905b7fae2c2228b19b9e43c0f221a8f2a
Jeetendra chauhan <jeet4code@gmail.com>
2022-11-03 19:58:33 +0530

    add components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageCaptureComponent } from './image-capture/image-capture.component';
+import { ImageCaptureComponent } from './common/components/image-capture/image-capture.component';
+import { ImageProcessComponent } from './common/components/image-process/image-process.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { UserInfoComponent } from './common/components/user-info/user-info.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageCaptureComponent
+    ImageCaptureComponent,
+    ImageProcessComponent,
+    ProductsComponent,
+    UserInfoComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
